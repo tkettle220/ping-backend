e9dd428beab0b3a406f7fd964d8931ae01b85037
Rebekah Liu <liu.rebekah@gmail.com>
2017-10-12 18:31:42 -0700

    stupid

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -7,7 +7,7 @@ class ChatChannel < ApplicationCable::Channel
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
-
+  #
   # def receive(payload)
   #   message = Message.create(chatroom_id: payload["chatroom_id"], content: payload["message"])
   #   ActionCable.server.broadcast('messages', {message: message.content, chatroom_id: message.chatroom_id})
